56c147a960e858b2d18d38068b4c04848fe7806b
pooja soni <poojasoni7024@gmail.com>
2024-02-27 23:26:29 +0530

    vendor model and entiti

---
+++ Shiva Enterprise APIs/Model/VendorModel.cs
@@ -0,0 +1,22 @@
+﻿namespace Shiva_Enterprise_APIs.Model
+{
+    public class VendorModel
+    {
+        public string VendorCode { get; set; }
+        public string VendorName { get; set; }
+        public string VendorType { get; set; }
+        public string VendorAddress { get; set; }
+        public string Phoneno { get; set; }
+        public string Email { get; set; }
+        public Guid  BankId{ get; set; }
+        public Guid TaxId { get; set; }
+        public DateTime ContractStartDate { get; set; }
+        public DateTime ContractEndDate { get; set; }
+        public string? Remark { get; set; }
+        public bool IsActive { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime CreatedDateTime { get; set; }
+        public string? ModifiedBy { get; set; }
+        public DateTime? ModifiedDateTime { get; set; }
+    }
+}
